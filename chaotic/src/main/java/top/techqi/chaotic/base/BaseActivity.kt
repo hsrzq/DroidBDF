b25dacb39c66a89555dd5ac77fc2d343f20c2b16
TechQI <dev@techqi.top>
2018-09-08 18:15:46 +0800

    在Activity中记录生命周期状态

--- chaotic/src/main/java/top/techqi/chaotic/base/BaseActivity.kt
+++ chaotic/src/main/java/top/techqi/chaotic/base/BaseActivity.kt
@@ -9,6 +9,7 @@ import top.techqi.chaotic.util.LogU
 /**
  * Activity 基类
  */
+@Suppress("unused", "MemberVisibilityCanBePrivate")
 abstract class BaseActivity : AppCompatActivity() {
     @Suppress("PropertyName", "HasPlatformType")
     protected open val TAG = javaClass.simpleName
@@ -16,38 +17,60 @@ abstract class BaseActivity : AppCompatActivity() {
     @Suppress("PropertyName")
     protected open val DEBUG = BuildConfig.DEBUG
 
+    var created = false
+        private set
+    var started = false
+        private set
+    var resumed = false
+        private set
+    val paused
+        get() = !resumed
+    val stopped
+        get() = !started
+    val destroyed
+        get() = !created
+
+    var restarted = 0
+        private set
+
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
         if (DEBUG) LogU.vv(TAG, "onCreate")
         super.onCreate(savedInstanceState)
+        created = true
     }
 
     @CallSuper
     override fun onStart() {
         if (DEBUG) LogU.vv(TAG, "onStart")
         super.onStart()
+        started = true
     }
 
     @CallSuper
     override fun onResume() {
         if (DEBUG) LogU.vv(TAG, "onResume")
         super.onResume()
+        resumed = true
     }
 
     @CallSuper
     override fun onPause() {
+        resumed = false
         super.onPause()
         if (DEBUG) LogU.vv(TAG, "onPause")
     }
 
     @CallSuper
     override fun onStop() {
+        started = false
         super.onStop()
         if (DEBUG) LogU.vv(TAG, "onStop")
     }
 
     @CallSuper
     override fun onDestroy() {
+        created = false
         super.onDestroy()
         if (DEBUG) LogU.vv(TAG, "onDestroy")
     }
@@ -56,5 +79,6 @@ abstract class BaseActivity : AppCompatActivity() {
     override fun onRestart() {
         if (DEBUG) LogU.vv(TAG, "onRestart")
         super.onRestart()
+        restarted++
     }
 }
